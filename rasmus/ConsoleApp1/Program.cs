878db38ad0e839c78abe38bce06fca047a5a2fa2
Opilane TTHK <opilane@tthk.ee>
2025-12-18 09:33:11 +0200

    sõne
    sõne kasutamine

---
+++ rasmus/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace sõned
+{
+    internal class program
+    {
+       static void Maiń(string[] args)
+        {
+            //string lause = "tere hommikust, mul on monster otsas, kas ma võin veel magada?";
+            //console.writeline("lause pikkus on: "+lause.lenght);
+            //console.writeline("see tekst on capslockis:"+lause.toUpper());
+            //console.writeline("see tekst on väiketähtedes:"+lause.toLower());
+            //console.writeline("kas lauses on sõna \"magada\"?:+lause.toLower().contains("magada"));
+
+            //list<string> list = new list<string>() {"auto","kraav","kruus"};
+            //string listkoos = "";
+            //foreach (var sõne in list)
+            //{
+            //      listkoos += (" "+sõne.toUpper()+",");
+            //}
+            //console.writeline(listkoos);
+
+            //string esinimi = "julius";
+            //string perekonnanimi = "cesar";
+            //string amet = "ohver";
+            //string kuuluskeiser = string.concat(esinimi,perekonnanimi,amet);
+            //console.writeline(kuuluskeiser);
+
+            //string kuuluskeiser2 = $"{eesnimi}, {perekonnanimi}, {amet};
+            //console.writeline(${eesnimi}, {perekonnanimi}, {amet};
+            //console.writeline(kuuluskeiser2);
+
+            //string[] sõnadlauses = lause.split(' ');
+            //foreach (var sõne in sõnalauses)
+            //{
+            //     console.writeline(sõne);
+            //}
+            //string ükssõna = "kuulilennuteetunneliluuk";
+            //console.writeline(ükssõna[4]);
+            //console.writeline(ükssõna.indexOF("i"));
+
+            //console.writeline(" {" ");
+
+            //console.writeline("a\nb");
+
+            //console.writeline("AAAA\teeeeeee");
+        }
+    }
+}
